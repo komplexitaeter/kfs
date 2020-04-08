2c5f3eda8c2fa2a26d660560affc565690e82b4d
francoiskomplex <francois.canonne@komplexitaeter.de>
2020-04-08 14:42:16 +0200

    'initial'

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'off');
+ini_set('log_errors', 'on');
+
+define('_MYSQL_HOST', 'localhost');
+define('_MYSQL_USER', 'root');
+define('_MYSQL_PWD', 'root');
+define('_MYSQL_DB', 'kfs_local');
+define('_MYSQL_PORT', 3306);
