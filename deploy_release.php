81701cd20a2062102e457ea919a501e3eeda6559
francoiskomplex <francois.canonne@komplexitaeter.de>
2020-04-08 12:37:47 +0200

    'initial'

--- deploy_release.php
+++ deploy_release.php
@@ -1,6 +1,12 @@
 <?php
 
-echo shell_exec("rm -rf kfs_release");
+echo exec("pwd", $op, $v);
+echo "Error:<br/>";
+print_r($op);
+echo "<br/>";
+echo "return_var<br/>";
+print_r($v);
+//echo shell_exec("rm -rf kfs_release");
 //print_r($op);
 //exec("git clone -b release https://github.com/komplexitaeter/kfs kfs_release");
 //exec("rm -rf kfs_release/.git");
