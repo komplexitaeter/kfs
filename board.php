c6acd4a03ca34159663ba89ced78f13ebaca22fd
francoiskomplex <francois.canonne@komplexitaeter.de>
2020-04-16 17:57:27 +0200

    'start simulation now possible'

--- board.php
+++ board.php
@@ -5,21 +5,28 @@
     <title>Board Kanban Ship Building Flow Simulation Online</title>
     <script src="./lib/fabric.js"></script>
     <script src="./script/flow.js"></script>
-    <link rel="stylesheet" type="text/css" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="./board.css">
 </head>
-<body onload="">
+<body>
 
-<div id="backlog"></div>
-<div id="controls"></div>
-<div id="observers"></div>
-<div id="stations">
-    <div id="1" class="station"></div>
-    <div id="2" class="station"></div>
-    <div id="3" class="station"></div>
-    <div id="4" class="station"></div>
-    <div id="done"></div>
+
+<div id="backlog">
+    <div id="item_id" class="item">&nbsp;</div>
 </div>
-<div id="workspace"></div>
+<div id="controls">&nbsp;</div>
+<div id="observers">
+    <div id="current_user" class="attendee">&nbsp;</div>
+    <div id="session_key" class="attendee">&nbsp;</div>
+</div>
+<div id="stations">
+    <div id="1" class="station">&nbsp;</div>
+    <div id="2" class="station">&nbsp;</div>
+    <div id="3" class="station">&nbsp;</div>
+    <div id="4" class="station">&nbsp;</div>
+    <div id="done" class="station">&nbsp;</div>
+</div>
+<div id="workspace" class="">&nbsp;</div>
+
 
 </body>
 </html>
--- update_simulation.php
+++ update_simulation.php
@@ -25,6 +25,15 @@ if(isset($_GET['status_code'])){
     $status_code = filter_input(INPUT_GET, 'status_code');
     if (strlen($status_code)>0) {
         array_push($sql_set, "status_code = '".$status_code."'");
+        if($status_code=="RUNNING"){
+            $sql = "DELETE FROM kfs_attendees_tbl WHERE TIMESTAMPDIFF( SECOND, last_callback_date, CURRENT_TIMESTAMP) >= 30 AND simulation_id=".$simulation_id;
+            if(!$result = $link->query($sql))
+            {
+                if ($link->connect_errno) {
+                    printf("\n Fail: %s\n", $link->connect_error);
+                    exit();
+                }
+        }
     }
 }
 
@@ -41,7 +50,6 @@ for($i=0; $i<count($sql_set);$i++){
 }
 
 $sql = "UPDATE kfs_simulation_tbl SET ".$sql_update." WHERE simulation_id='".$simulation_id."'";
-echo "$sql";
 if(!$result = $link->query($sql))
 {
     if ($link->connect_errno) {
