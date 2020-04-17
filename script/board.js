a006adc42a1606dc22466a6d3281fdcf085c3cb0
andreaskomplex <Pobhij-dunbu7-fudjev>
2020-04-17 11:08:21 +0200

    small surprise

--- script/board.js
+++ script/board.js
@@ -60,6 +60,7 @@ function createAttendeeDiv(obj, session_key){
     myDiv.classList.add("attendee");
     if(obj.session_key == session_key){
         myDiv.classList.add("current_user");
+        dragElement(myDiv);
     }
     else{
         myDiv.classList.add("not_current_user");
@@ -67,4 +68,43 @@ function createAttendeeDiv(obj, session_key){
     myDiv.id = obj.session_key;
     myDiv.innerHTML = '<button class="avatar">&nbsp;</button>'+obj.name;
     return myDiv;
+}
+
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+        elmnt.style.position = 'absolute';
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+        elmnt.style.position = 'reset'; /*todo*/
+    }
 }
