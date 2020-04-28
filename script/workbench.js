e61682adefc61d318a32ce8cbdde607ba677e051
andreaskomplex <Pobhij-dunbu7-fudjev>
2020-04-28 09:09:17 +0200

    removed some comments

--- script/workbench.js
+++ script/workbench.js
@@ -4,13 +4,6 @@ class Workbench {
     constructor() {
     }
     setContext(implParam, stationId) {
-        /*
-        Konstruktor:
-            (beim Aufruf werden Canvasobject, Toolbox und Instruction resettet
-            , es ist erstmal der Zustand: Kein Item in Arbeit
-            , merkt sich um welche Station id es grade geht)
-
-          */
 
         this.implParam = implParam;
         this.stationId = stationId;
@@ -67,32 +60,6 @@ class Workbench {
     start(item_svg){
     }
 
-
-
-    /*
-
-     StartWorkOnItem:
-     (stellt das aktuelle Schiff entsprechend der Arbeitsstation dar und
-     registriert Eventhandler, damit am Schiff gearbeitet werden kann
-     ,merkt sich an welchem Item-Id grade gearbeitet wird
-     )
-     - ID des Items
-     - SVG welches den aktuellen Stand des Schiffs repräsentiert
-
-     FinishWork:
-     (Prüft, ob der Arbeitschritt erfolgreich abgeschlossen wurde
-     , wenn ja wird analog Construktor alle resettet)
-     -> Erfolg: liefert SVG des aktuellen Stand des Schiffs nach dem Bearbeitungsschritt
-     -> Misserfolg: Anweisung an den Anwender, was das Problem ist
-
-     IsItemInProcess
-     - ID des Items
-     -> liefert True, wenn Item grade in Arbeit ist, sonst false
-
-     GetThumbnailSvg
-     -> liefert SVG, welches den aktuelen Arbeitsbereich repräsentiert
-
-      */
 }
 
 function loadWorkbench(implName, implParam, stationId) {
